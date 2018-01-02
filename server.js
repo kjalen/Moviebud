1b940c03bfcf4b148bd586d4497d33a89e0f3725
kjalen <kjalen@uvic.ca>
2018-01-01 22:43:24 -0800

    moved fetch json movie data into its own function

--- server.js
+++ server.js
@@ -26,38 +26,63 @@ app.listen(port, () => {
 
 
 app.get('/', (req, res) => {
-    let url = `${base_url}/movies?apikey=${apikey}&limit=10&fields=id,slug,title,poster_image_thumbnail,original_title,synopsis,genres,poster_image`;
-    fetch(url).then(response=>{
-      response.json().then(json =>{
-        res.render('index', {
-          title: 'Moviez!',
-          message: 'Heres dem moviez',
-          json: json.movies
-        })
+  let url = `${base_url}/movies?apikey=${apikey}&limit=10&fields=id,slug,title,poster_image_thumbnail,original_title,synopsis,genres,poster_image`;
+  fetch(url).then(response=>{
+    response.json().then(json =>{
+      res.render('index', {
+        title: 'Moviez!',
+        message: 'Heres dem moviez',
+        json: json.movies
       })
-    }).catch(error =>{
-      console.log(error);
     })
-
-
-
+  }).catch(error =>{
+    console.log(error);
+  })
 });
 
 app.get('/movies/:id', (req, res) => {
-
-  let movie_url = `${base_url}/movies/${req.params.id}?apikey=${apikey}`;
-  fetch(movie_url)
-    .then(response => {
-      // response is the movie json, send it to the client
-        response.json().then(json =>{
-          res.render('movies', {
-            title: json.movie.title,
-            movie_json: json.movie
-          });
-      });
-
-    }).catch(error =>{
-      console.log(error);
-    })
-
+  let movie_json;
+  let showtimes_json;
+  get_movie_json(req.params.id).then(result=>{
+    res.render('movies', {
+      title: result.title,
+      movie_json: result.movie
+    });
+  })
 });
+
+function get_movie_json(movie_id){
+  let movie_url = `${base_url}/movies/${movie_id}?apikey=${apikey}`;
+  return new Promise((resolve, reject) => {
+    fetch(movie_url)
+      .then(response => {
+        // response is the movie json, send it to the client
+          response.json().then(json =>{
+            return resolve(json)
+        });
+      }).catch(error =>{
+        return reject(error);
+      })
+  })
+
+}
+
+ // https://api.internationalshowtimes.com/v4/showtimes/?apikey=iCgCjwBWEmcyGmdZfcRiPfJE855MHvpc&city_ids=4978&movie_id=16216&cinema_id=48123
+function get_movie_showtimes(movie_id){
+  let city_id = 4978; // Victoria
+  let cinema_id = 48123;
+  let showtime_url = `${base_url}/showtimes/?apikey=${apikey}&movie_id=${movie_id}&city_id=${city_id}&cinema_id=${cinema_id}`;
+  return new Promise((resolve, reject) => {
+    fetch(showtime_url)
+      .then(response => {
+        // response is the movie json, send it to the client
+          response.json().then(json =>{
+            return resolve(json)
+        });
+
+      }).catch(error =>{
+        return reject(error);
+      })
+  })
+
+}
